af464ad1569caf1b9f4237f9125b48eecfe19d71
Shashwat Pathak <39920958+D-extreme@users.noreply.github.com>
2021-08-29 05:45:01 +0530

    Final Commit before Submission

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+# Author: Shashwat Pathak
+
 from flask import Flask
 
+# Folder for saving the enhancements
 UPLOAD_FOLDER = 'C:/Users/shash/PycharmProjects/ImageProcessingAssignmentGUI/static/uploads/'
+
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['PROFILE_PIC'] = 'profile_pic.jpg'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # Maximum content dimension
